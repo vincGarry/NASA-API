cc3c530b7524fe92424878e4e87be42a5ddb074f
Vincentius Garry Wijaya <vincgarry@gmail.com>
2024-03-16 15:31:52 +0700

    initial commit boilerplate code for spring boot

---
+++ NASA-API/src/main/java/com/example/NASAAPI/NasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.NASAAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApiApplication.class, args);
+	}
+
+}
---
+++ NASA-API/src/test/java/com/example/NASAAPI/NasaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NASAAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
